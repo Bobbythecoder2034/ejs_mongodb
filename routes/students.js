979eb2fbbac3ec28f0795887ef70067a6d622982
Bobbythecoder2034 <kkoala110@gmail.com>
2025-10-21 15:01:13 -0700

    beans

---
+++ routes/students.js
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+
+const router = Router()
+
+
+
+// Homepage: GET /students?search=tem$cohort=Year%201
+router.get('/', async(req,res)=>{
+    
+})
+
+export default router
